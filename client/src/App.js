75f1ac86b291ff47460f36a141024edb52afa287
juanitorreslp <devjitorres@gmail.com>
2025-01-07 18:10:58 -0300

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,18 @@
 import './App.css';
+import Header from './Header';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import Post from './Post';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">The Goat Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img src="https://media.0221.com.ar/p/6028a926bc14aeb98cf87b26f05bd3ba/adjuntos/357/imagenes/100/144/0100144038/1400x0/smart/estudiantes-newells-mendez-goljpg.jpg" alt="Blog Image" />
-        </div>
-        <div className="text">
-          <h2>This is the first blog entry by elgoat16. Don't leave us MM16!</h2>
-          <p className="info">
-            <a className="author">elgoat16</a>
-            <time>2025-01-07 16:55</time>
-          </p>
-          <p className="summary">La frase de Alejandro Sabella no pasa inadvertida. Está ahí, bien visible, casi observándolo todo, en un lugar destacado de la sala de kinesiología del Country, el lugar donde los jugadores acuden a recuperarse de sus lesiones. No es casual, claro. Busca fortalecer el temple, dar un mensaje positivo, inspirar, alentar, motivar. Y eso le pasó a Mauro Méndez, el uruguayo que se sigue recuperando de una grave lesión en su rodilla izquierda.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://media.0221.com.ar/p/6028a926bc14aeb98cf87b26f05bd3ba/adjuntos/357/imagenes/100/144/0100144038/1400x0/smart/estudiantes-newells-mendez-goljpg.jpg" alt="Blog Image" />
-        </div>
-        <div className="text">
-          <h2>This is the first blog entry by elgoat16. Don't leave us MM16!</h2>
-          <p className="info">
-            <a className="author">elgoat16</a>
-            <time>2025-01-07 16:55</time>
-          </p>
-          <p className="summary">La frase de Alejandro Sabella no pasa inadvertida. Está ahí, bien visible, casi observándolo todo, en un lugar destacado de la sala de kinesiología del Country, el lugar donde los jugadores acuden a recuperarse de sus lesiones. No es casual, claro. Busca fortalecer el temple, dar un mensaje positivo, inspirar, alentar, motivar. Y eso le pasó a Mauro Méndez, el uruguayo que se sigue recuperando de una grave lesión en su rodilla izquierda.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://media.0221.com.ar/p/6028a926bc14aeb98cf87b26f05bd3ba/adjuntos/357/imagenes/100/144/0100144038/1400x0/smart/estudiantes-newells-mendez-goljpg.jpg" alt="Blog Image" />
-        </div>
-        <div className="text">
-          <h2>This is the first blog entry by elgoat16. Don't leave us MM16!</h2>
-          <p className="info">
-            <a className="author">elgoat16</a>
-            <time>2025-01-07 16:55</time>
-          </p>
-          <p className="summary">La frase de Alejandro Sabella no pasa inadvertida. Está ahí, bien visible, casi observándolo todo, en un lugar destacado de la sala de kinesiología del Country, el lugar donde los jugadores acuden a recuperarse de sus lesiones. No es casual, claro. Busca fortalecer el temple, dar un mensaje positivo, inspirar, alentar, motivar. Y eso le pasó a Mauro Méndez, el uruguayo que se sigue recuperando de una grave lesión en su rodilla izquierda.</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={'/login'} element= {<div>Login page</div>} />
+      </Route>
+    </Routes>
   );
 }
 
