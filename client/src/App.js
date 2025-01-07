7c79f4c7bda06ca015e623ca80a3a74ed13091c7
juanitorreslp <devjitorres@gmail.com>
2025-01-07 19:14:22 -0300

    Add login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './Header';
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import Post from './Post';
 import {Route, Routes} from "react-router-dom";
 
@@ -10,7 +12,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={'/login'} element= {<div>Login page</div>} />
+        <Route path={'/login'} element={<LoginPage />} />
+        <Route path={'/register'} element= {<RegisterPage />} />
       </Route>
     </Routes>
   );
--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 export default function Header() {
     return(
         <header>
-        <Link to="/" className="logo">The Goat Blog</Link>
+        <Link to="/" className="logo">The GOAT Blog</Link>
         <nav>
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
