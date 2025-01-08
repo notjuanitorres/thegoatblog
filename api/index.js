4ae179c5c029367d215ed6557fe3fcd2c3eeb2e0
juanitorreslp <devjitorres@gmail.com>
2025-01-07 22:49:12 -0300

    Add backend

---
+++ api/index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const salt = bcrypt.genSaltSync(10);
+const secret = 'ydoksakddsadasdSAKjsdakd';
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect('mongodb+srv://devjitorres:STVB9StOVSAEBhzh@cluster0.fcrv0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.post('/register', async (req, res) => {
+  const {username, password} = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch(e) {
+    console.log('Registration error:', e);
+    res.status(400).json(e);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  try {
+    const userDoc = await User.findOne({username});
+    if (!userDoc) {
+      return res.status(400).json('User not found');
+    }
+
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token, {
+          httpOnly: true,
+          sameSite: 'strict',
+          secure: process.env.NODE_ENV === 'production'
+        }).json({
+          id: userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('Wrong credentials');
+    }
+  } catch (err) {
+    console.error('Login error:', err);
+    res.status(500).json('Internal server error');
+  }
+});
+
+app.get('/profile', async (req, res) => {
+  const {token} = req.cookies;
+  if (!token) {
+    return res.status(401).json('No token');
+  }
+  
+  try {
+    const info = jwt.verify(token, secret);
+    res.json(info);
+  } catch (err) {
+    res.status(401).json('Invalid token');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '', {
+    expires: new Date(0),
+    httpOnly: true
+  }).json('Logged out');
+});
+
+app.listen(4000, () => {
+  console.log('Server running on port 4000');
+});
